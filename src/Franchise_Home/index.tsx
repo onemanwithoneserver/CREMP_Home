6ab817a27d3950ea9153b4538f46f9abd29b278c
OnemanwithOneserver <onemanwithoneserver@gmail.com>
2026-07-31 17:12:25 +0530

    Update index.tsx

--- src/Franchise_Home/index.tsx
+++ src/Franchise_Home/index.tsx
@@ -1,16 +1,16 @@
 import HeroGallery from "./01_HeroGallery";
 import HeroBottomDetails from "./02_HeroBottomDetails";
-import InvestmentSnapshot from "./04_InvestmentSnapshot";
+// import InvestmentSnapshot from "./04_InvestmentSnapshot";
 import FranchiseModels from "./05_FranchiseModels";
 import IdealPartner from "./07_IdealPartner";
 import FullCycleSupport from "./08_FullCycleSupport";
 import FounderStory from "./09_FounderStory";
-import MissionVision from "./10_MissionVision";
-import JourneyTimeline from "./11_JourneyTimeline";
-import ProvenPlaybook from "./12_ProvenPlaybook";
+// import MissionVision from "./10_MissionVision";
+// import JourneyTimeline from "./11_JourneyTimeline";
+// import ProvenPlaybook from "./12_ProvenPlaybook";
 import Leadership from "./13_Leadership";
 import MediaGallery from "./14_MediaGallery";
-import NumbersSpeak from "./15_NumbersSpeak";
+// import NumbersSpeak from "./15_NumbersSpeak";
 import FranchiseNetwork from "./16_FranchiseNetwork";
 import FAQ from "./17_FAQ";
 
@@ -24,17 +24,17 @@ export default function FranchiseHome({ isMobile }: FranchiseHomeProps) {
             <div className="flex-1 flex flex-col">
                 <HeroGallery isMobile={isMobile} />
                 <HeroBottomDetails isMobile={isMobile} />
-                <InvestmentSnapshot isMobile={isMobile} />
+                {/* <InvestmentSnapshot isMobile={isMobile} /> */}
                 <FranchiseModels isMobile={isMobile} />
                 <IdealPartner isMobile={isMobile} />
                 <FullCycleSupport isMobile={isMobile} />
                 <FounderStory isMobile={isMobile} />
-                <MissionVision isMobile={isMobile} />
-                <JourneyTimeline isMobile={isMobile} />
-                <ProvenPlaybook isMobile={isMobile} />
+                {/* <MissionVision isMobile={isMobile} /> */}
+                {/* <JourneyTimeline isMobile={isMobile} /> */}
+                {/* <ProvenPlaybook isMobile={isMobile} /> */}
                 <Leadership isMobile={isMobile} />
                 <MediaGallery isMobile={isMobile} />
-                <NumbersSpeak isMobile={isMobile} />
+                {/* <NumbersSpeak isMobile={isMobile} /> */}
                 <FranchiseNetwork isMobile={isMobile} />
                 <FAQ isMobile={isMobile} />
             </div>
