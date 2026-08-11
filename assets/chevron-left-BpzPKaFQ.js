b28807197e73c5bf37f0a271dc94ef58447fc388
OnemanwithOneserver <onemanwithoneserver@gmail.com>
2026-08-11 21:53:40 +0530

    Updates

--- assets/chevron-left-BpzPKaFQ.js
+++
@@ -1,1 +0,0 @@
-import{pt as e}from"./index-CeYJQO-W.js";var t=e(`chevron-left`,[[`path`,{d:`m15 18-6-6 6-6`,key:`1wnfg3`}]]);export{t};
---
+++ assets/navigation-CJ3WRCet.js
@@ -0,0 +1,1 @@
+import{pt as e}from"./index-77uVH5U3.js";var t=e(`navigation`,[[`polygon`,{points:`3 11 22 2 13 21 11 13 3 11`,key:`1ltx0t`}]]);export{t};
