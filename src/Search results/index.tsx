42468ed31581f0e03dd3e7ae54d92492041f4dd8
OnemanwithOneserver <onemanwithoneserver@gmail.com>
2026-08-22 22:02:00 +0530

    Update index.tsx

--- src/Search results/index.tsx
+++ src/Search results/index.tsx
@@ -1,9 +1,18 @@
 import Desktop from './desktop';
 import Mobile from './mobile';
+import SiteHeader from '../components/commonfiles/SiteHeader';
+import SiteFooter from '../components/commonfiles/SiteFooter';
 
 export default function SearchResults({ isMobile }: { isMobile?: boolean }) {
   if (isMobile) {
     return <Mobile />;
   }
-  return <Desktop />;
+
+  return (
+    <>
+      <SiteHeader />
+      <Desktop />
+      <SiteFooter />
+    </>
+  );
 }
