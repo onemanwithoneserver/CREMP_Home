5e97dfda31f5a3442f23efdc16e5c3d5fe765dbb
OnemanwithOneserver <onemanwithoneserver@gmail.com>
2026-08-31 10:14:30 +0530

    Updates

--- assets/OpenInNew-DEY92C6S.js
+++
@@ -1,1 +0,0 @@
-import{N as e}from"./createLucideIcon-Bc278XDu.js";import{E as t}from"./index-BG5RopRN.js";var n=t((0,e().jsx)(`path`,{d:`M19 19H5V5h7V3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-7h-2zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3z`}),`OpenInNew`);export{n as t};
