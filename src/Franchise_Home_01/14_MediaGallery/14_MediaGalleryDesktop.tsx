1eb9009c18d5c3dbade8a7cc3c65fb7c7ea445b8
OnemanwithOneserver <onemanwithoneserver@gmail.com>
2026-09-17 22:53:02 +0530

    .

--- src/Franchise_Home_01/14_MediaGallery/14_MediaGalleryDesktop.tsx
+++ src/Franchise_Home_01/14_MediaGallery/14_MediaGalleryDesktop.tsx
@@ -123,11 +123,10 @@ export default function MediaGalleryDesktop() {
                         key={item.id}
                         layout
                         variants={fadeScaleUp}
-                        initial="hidden"
-                        animate="show"
-                        exit={{ opacity: 0, scale: 0.9 }}
+                        whileHover={{ y: -5, scale: 1.02 }}
+                        exit={{ opacity: 0, scale: 0.9, y: 20 }}
                         className={clsx(
-                          "relative break-inside-avoid rounded-[4px] overflow-hidden group shadow-md hover:shadow-2xl bg-gray-900 border border-black/5 dark:border-white/10 cursor-pointer transform-gpu transition-all duration-500",
+                          "relative break-inside-avoid rounded-[4px] overflow-hidden group shadow-md hover:shadow-2xl bg-gray-900 border border-black/5 dark:border-white/10 cursor-pointer transform-gpu transition-all duration-300",
                           heightClass
                         )}
                       >
